ef4b3023e054828ed1536389b33a1edcffd2641f
Carlos Baraza Haro <carlos.baraza@ieee.org>
2021-11-22 10:54:36 +0000

    Add title and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import React from "react";
 import { Layout } from "../components/Layout/Layout";
 import { Tasks } from "../components/Tasks/Tasks";
 
 export const Home: NextPage = (props) => {
   return (
     <>
+      <Head>
+        <title>FocusKeep | Track daily work habits</title>
+        <meta
+          name="description"
+          content="Track daily work routine with blocks of focused time. Pomodoro technique. Forever free."
+        />
+      </Head>
+
       <Layout>
         <div className="Home">
           <Tasks />
