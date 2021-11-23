a6ede257ed6ae9a95b3ccfd6f92ad93c7ef59463
Carlos Baraza Haro <carlos.baraza@ieee.org>
2021-11-23 21:06:56 +0000

    Fix caching prod

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
     dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
   },
 });
